5e06559b580dabed59210ff2539cfd9eb9ceafff
Steve Mullins <sgmullins@gmail.com>
2020-01-21 16:15:32 -0700

    add dataloader and cofigure to batch queries

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,6 +1,15 @@
 const Event = require("../../models/event");
 const User = require("../../models/user");
 const { dateToString } = require("../../helpers/date");
+const DataLoader = require("dataloader");
+
+const eventLoader = new DataLoader(eventIds => {
+  return events(eventIds);
+});
+
+const userLoader = new DataLoader(userIds => {
+  return User.find({ _id: { $in: userIds } });
+});
 
 const events = async eventIds => {
   try {
@@ -14,19 +23,19 @@ const events = async eventIds => {
 };
 const singleEvent = async eventId => {
   try {
-    const event = await Event.findById(eventId);
-    return transformEvent(event);
+    const event = await eventLoader.load(eventId.toString()); //to string solves event load issue with objects
+    return event;
   } catch (err) {
     throw err;
   }
 };
 const user = async userId => {
   try {
-    const user = await User.findById(userId);
+    const user = await userLoader.load(userId.toString());
     return {
       ...user._doc,
       _id: user.id,
-      createdEvents: events.bind(this, user._doc.createdEvents)
+      createdEvents: eventLoader.load.bind(this, user._doc.createdEvents)
     };
   } catch (err) {
     throw err;
